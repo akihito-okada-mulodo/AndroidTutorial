95daebb76224e11ee3fb60949ce5b9474bed5e35
akihito <akihito.okada@mulodo.com>
2014-12-15 23:08:41 +0900

    initial commit

---
+++ AndroidTutorial/app/src/main/java/com/tutorial/okadaakihito/androidtutorial/SecondActivity.java
@@ -0,0 +1,49 @@
+package com.tutorial.okadaakihito.androidtutorial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+/**
+ * Created by okadaakihito on 2014/12/15.
+ */
+public class SecondActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 0:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        finish();
+                        break;
+                }
+                break;
+        }
+    }
+
+    public void selectedButton(View view) {
+        int id = view.getId();
+
+        switch(id) {
+            case R.id.push:
+                Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
+                intent.putExtra("KEYWORD", "keywordStr");
+                startActivityForResult(intent, 0);
+                overridePendingTransition(0, 0);
+                break;
+        }
+    }
+
+}
