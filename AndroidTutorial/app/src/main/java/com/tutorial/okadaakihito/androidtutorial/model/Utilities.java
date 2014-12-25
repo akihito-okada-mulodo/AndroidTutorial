cb0b54e8ef529140527c374a90ceac16f0d5a55a
akihito <akihito.okada@mulodo.com>
2014-12-25 18:44:29 +0900

    add contents menu page

---
+++ AndroidTutorial/app/src/main/java/com/tutorial/okadaakihito/androidtutorial/model/Utilities.java
@@ -0,0 +1,14 @@
+package com.tutorial.okadaakihito.androidtutorial.model;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.tutorial.okadaakihito.androidtutorial.MainActivity;
+import com.tutorial.okadaakihito.androidtutorial.ThirdActivity;
+
+/**
+ * Created by okadaakihito on 2014/12/25.
+ */
+public class Utilities {
+
+}
